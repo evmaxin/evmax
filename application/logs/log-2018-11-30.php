77621682e53e0746814b634313e71896ff8563e7
evmaxin <nagsky1@gmail.com>
2018-11-30 15:10:51 +0530

    Signed-off-by: evmaxin <nagsky1@gmail.com>

---
+++ application/logs/log-2018-11-30.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> No URI present. Default controller set.
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0437
