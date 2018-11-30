8f727a9db6e63de3cdf369a9d26e485968534274
evmaxin <nagsky1@gmail.com>
2018-11-30 14:42:38 +0530

    log files chnaged
    Signed-off-by: evmaxin <nagsky1@gmail.com>

--- application/logs/log-2018-11-30.php
+++ application/logs/log-2018-11-30.php
@@ -30,3 +30,35 @@ DEBUG - 2018-11 --> Total execution time: 0.0408
 DEBUG - 2018-11 --> UTF-8 Support Enabled
 DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
 DEBUG - 2018-11 --> Total execution time: 0.0320
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> No URI present. Default controller set.
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.2402
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0274
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.1458
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0659
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> No URI present. Default controller set.
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0328
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> No URI present. Default controller set.
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0325
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> No URI present. Default controller set.
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0490
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> No URI present. Default controller set.
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0570
+DEBUG - 2018-11 --> UTF-8 Support Enabled
+DEBUG - 2018-11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-11 --> Total execution time: 0.0423
