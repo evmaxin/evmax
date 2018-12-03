0ad98ec596449274e564111c15241db4c7615699
evmaxin <nagsky1@gmail.com>
2018-12-03 10:56:49 +0530

    index.php in base url removed

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://localhost:1337/evmax/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
